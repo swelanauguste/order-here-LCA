72dd67121a6ca8a98c1b0442bebc47e8f962a877
Swelan Auguste <swelanauguste@gmail.com>
2022-10-25 09:36:45 -0400

    added bootstrap

--- app/restaurants/views.py
+++ app/restaurants/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    # get_ip = os.system("curl ipinfo.io/ip")
-    # print(get_ip)
     ip = request.META.get('REMOTE_ADDR')
-    print(ip)
     context = {"ip": ip}
     return render(request, "index.html", context)
